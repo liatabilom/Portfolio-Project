0a9d1d7a1b2e749aaff7836f93ce84ad1224ced1
Lia <lia.tabilo@gmail.com>
2019-07-24 16:47:19 +0100

    Added fontAwesome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "font-awesome/css/font-awesome.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <h1>
+        <header className="header .d-sm-flex mt-3">
+          Nádia
+          <p>Correia Gonçalves</p>
+        </header>
+      </h1>
+      <div>
+        <form>
+          <div className="float-left">
+            <input className="emailInput" type="text" placeholder="Email" />
+          </div>
+          <div className="float-left ml-2">
+            <i class="fas fa-info-circle" size="lg" />
+          </div>
+          <div className="float-left ml-2" />
+        </form>
+      </div>
+      <h2>
+        <div className="row float-left mt-4">
+          <div className="col-sm-4">One of three columns</div>
+          <div className="col-sm-4">One of three columns</div>
+          <div className="col-sm-4">One of three columns</div>
+        </div>
+      </h2>
     </div>
   );
 }
